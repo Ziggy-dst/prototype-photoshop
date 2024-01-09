618fa925c4d847fae420af7a45ff3794c210b512
blaerzhang <zhangyueyang0022@126.com>
2024-01-10 05:19:41 +0800

    Finished DotAbility(Feedbacks Unfinished); Added AudioManager

---
+++ prototype-photoshop/Assets/Scripts/Abilities/DotAbility.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Abilities;
+using UnityEngine;
+
+namespace CustomNamespace
+{
+    public class DotAbility : AbilityBase
+    {
+        public float dotSize;
+        public float damage; //not necessary
+        public Sprite dotCursorSprite;
+
+        [Header("Auto Click")] 
+        public bool autoClick;
+        public float autoClickTimeThreshold;
+        public float autoClickFrequency;
+        private float holdTimer = 0;
+
+        [Header("Feedbacks")]
+        public AudioClip soundFX;
+
+
+        public override void OnKeyModifierPressed()
+        {
+            base.OnKeyModifierPressed();
+            //TODO: Change Cursor
+        }
+
+        public override void OnKeyModifierReleased()
+        {
+            base.OnKeyModifierReleased();
+            //TODO: Change Cursor Back
+        }
+
+        public override void OnKeyTriggerPressed()
+        {
+            base.OnKeyTriggerPressed();
+
+            Collider2D[] clickedEnemies = Physics2D.OverlapCircleAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), dotSize);
+            foreach (var enemyCollider2D in clickedEnemies)
+            {
+                enemyCollider2D.GetComponent<Enemy>().Dead(); //Instant Kill, to be modified
+            }
+            
+            AudioManager.instance.PlaySound(soundFX);
+        }
+
+        public override void OnKeyTriggerHolding()
+        {
+            base.OnKeyTriggerHolding();
+            
+            holdTimer += Time.deltaTime;
+
+            if (holdTimer >= autoClickTimeThreshold)
+            {
+                holdTimer -= autoClickFrequency;
+                if (autoClick) OnKeyTriggerPressed();
+            }
+        }
+
+        public override void OnKeyTriggerReleased()
+        {
+            base.OnKeyTriggerReleased();
+            holdTimer = 0;
+        }
+    }
+}
