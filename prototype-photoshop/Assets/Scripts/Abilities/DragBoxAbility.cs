bed95983de214c6fce9e05eb9429a1f3bc780f5f
blaerzhang <zhangyueyang0022@126.com>
2024-01-11 16:22:52 -0500

    Finished Drag Box Ability

--- prototype-photoshop/Assets/Scripts/Abilities/DragBoxAbility.cs
+++ prototype-photoshop/Assets/Scripts/Abilities/DragBoxAbility.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Abilities;
@@ -7,14 +8,26 @@ namespace CustomNamespace
 {
     public class DragBoxAbility : AbilityBase
     {
-        public float diagonalLength;
+        private Vector2 origin;
+        private GameObject selectionBoxInstance;
+        private float currentDiagonalLength;
+        private LineRenderer rangeArcRenderer;
+        private GameObject rangeArcHolder;
+        
+        public float maxDiagonalLength;
         public float damage; //not necessary
         public GameObject selectionBoxPrefab;
 
+        [Header("Range Arc")]
+        public int segments;
+        public Material lineMaterial;
+        public float lineWidth;
+        public int sortingOrder;
+
         [Header("Feedbacks")]
         public AudioClip soundFX;
-
-
+        
+        
         public override void OnKeyModifierPressed()
         {
             base.OnKeyModifierPressed();
@@ -31,7 +44,12 @@ namespace CustomNamespace
         {
             base.OnKeyTriggerPressed();
             
-            
+            origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            selectionBoxInstance = Instantiate(selectionBoxPrefab, origin, Quaternion.Euler(Vector3.zero));
+            selectionBoxInstance.transform.localScale = Vector2.zero;
+            currentDiagonalLength = 0;
+
+            ShowRange();
             // AudioManager.instance.PlaySound(soundFX);
         }
 
@@ -39,13 +57,74 @@ namespace CustomNamespace
         {
             base.OnKeyTriggerHolding();
             
-            
+            Vector2 endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float boxWidth = endPos.x - origin.x;
+            float boxHeight = origin.y - endPos.y;
+
+            currentDiagonalLength = Mathf.Sqrt(boxWidth * boxWidth + boxHeight * boxHeight);
+            if (currentDiagonalLength <= maxDiagonalLength)
+            {
+                selectionBoxInstance.transform.localScale = new Vector2(boxWidth, boxHeight);
+            }
+            else
+            {
+                selectionBoxInstance.transform.localScale =
+                    new Vector2(boxWidth, boxHeight) * (maxDiagonalLength / currentDiagonalLength);
+            }
+
+            FlipRange(boxWidth, boxHeight);
         }
 
         public override void OnKeyTriggerReleased()
         {
             base.OnKeyTriggerReleased();
+            
+            Destroy(rangeArcHolder);
+            List<Collider2D> selectedEnemies = new List<Collider2D>();
+            selectionBoxInstance.GetComponent<BoxCollider2D>().GetContacts(selectedEnemies);
+            foreach (var enemy in selectedEnemies)
+            {
+                enemy.GetComponent<Enemy>().Dead();
+            }
+            
+            Destroy(selectionBoxInstance);
+        }
 
+        private void ShowRange()
+        {
+            rangeArcHolder = new GameObject("Range Arc Holder");
+            rangeArcHolder.transform.position = origin;
+            
+            rangeArcRenderer = rangeArcHolder.AddComponent<LineRenderer>();
+            rangeArcRenderer.useWorldSpace = false;
+            rangeArcRenderer.startWidth = lineWidth;
+            rangeArcRenderer.material = lineMaterial;
+            rangeArcRenderer.sortingOrder = sortingOrder;
+            rangeArcRenderer.positionCount = segments + 1;
+            
+
+            float angle = 95;
+            float arcLength = 175 - 95;
+            for (int i = 0; i <= segments; i++)
+            {
+                float x = Mathf.Sin(Mathf.Deg2Rad * angle) * maxDiagonalLength;
+                float y = Mathf.Cos(Mathf.Deg2Rad * angle) * maxDiagonalLength;
+                
+                rangeArcRenderer.SetPosition(i, new Vector2(x, y));
+
+                angle += (arcLength / segments);
+            }
+        }
+
+        private void FlipRange(float boxWidth, float boxHeight)
+        {
+            float localScaleX = 1;
+            float localScaleY = 1;
+
+            localScaleX = boxWidth >= 0 ? 1 : -1;
+            localScaleY = boxHeight >= 0 ? 1 : -1;
+
+            rangeArcHolder.transform.localScale = new Vector2(localScaleX, localScaleY);
         }
     }
 }
